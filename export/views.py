bef85b740d602798840d0351e0f2c561ea46dd1c
Faishal Manzar <faishalmanzar@gmail.com>
2021-11-16 20:30:04 +0530

    Initial commit

--- export/views.py
+++ export/views.py
@@ -1,16 +1,20 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from .resource import ExportResource
+
+def home_view(request, *args, **kwargs):
+    return render(request, "home.html")
 
 def csv_export(request, *args, **kwargs):
     csv_resource = ExportResource()
     dataset = csv_resource.export()
     response = HttpResponse(dataset.csv, content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="persons.csv"'
+    response['Content-Disposition'] = 'attachment; filename="data-export.csv"'
     return response
 
 def excel_export(request, *args, **kwargs):
     excel_resource = ExportResource()
     dataset = excel_resource.export()
     response = HttpResponse(dataset.xls, content_type='application/vnd.ms-excel')
-    response['Content-Disposition'] = 'attachment; filename="persons.xls"'
+    response['Content-Disposition'] = 'attachment; filename="data-export.xls"'
     return response
--- src/urls.py
+++ src/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from export.views import csv_export, excel_export
+from export.views import csv_export, excel_export, home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('export/csv/', csv_export, name='csv-eport'),
-    path('export/excel/', excel_export, name='excel-eport'),
+    path('', home_view, name='home'),
+    path('export/csv/', csv_export, name='csv-export'),
+    path('export/excel/', excel_export, name='excel-export'),
 ]
